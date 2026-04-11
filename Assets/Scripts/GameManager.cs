c83fb334bddd02ff659e87f987e105073f8a9ca1
sbm5661 <sbm5661@gmail.com>
2026-04-11 00:05:25 -0600

    random mimic assigning

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,15 +3,21 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    private bool gameEnded = false;
+
     [SerializeField] Visitor[] visitors;
     [SerializeField] private CallManager callManager;
 
     private CharacterData[] characters;
     private int currentIndex = 0;
 
+    [SerializeField] private int mimicCount = 2;
+    private bool[] mimicFlags;
+
     private void Start()
     {
         CreateCharacters();
+        AssignMimics();
         StartCurrentCall();
     }
 
@@ -24,26 +30,47 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    private void AssignMimics()
+    {
+        mimicFlags = new bool[characters.Length];
+
+        int assigned = 0;
+
+        while (assigned < mimicCount)
+        {
+            int randomIndex = Random.Range(0, characters.Length);
+
+            if (!mimicFlags[randomIndex])
+            {
+                mimicFlags[randomIndex] = true;
+                assigned++;
+            }
+        }
+    }
+
     public void StartCurrentCall()
     {
         CharacterData selectedCharacter = characters[currentIndex];
+        bool isMimic = mimicFlags[currentIndex];
 
-        bool isMimic = false;
-        if (currentIndex == 1)  // second character mimic for now
-        {
-            isMimic = true;
-        }
+        Debug.Log("Current caller: " + selectedCharacter.Name + " | Mimic: " + isMimic);
 
         callManager.StartCall(selectedCharacter, isMimic);
     }
 
     public void NextCall()
     {
+        if (gameEnded)
+            return;
+
         currentIndex++;
 
         if (currentIndex >= characters.Length)
         {
-            currentIndex = 0;
+            gameEnded = true;
+            Debug.Log("No more callers.");
+            // TODO: show end screen / score / game over
+            return;
         }
 
         StartCurrentCall();
