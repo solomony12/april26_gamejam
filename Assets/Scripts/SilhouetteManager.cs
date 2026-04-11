5427cd25b430d7736d1019cb7e453ed9478c8173
sbm5661 <sbm5661@gmail.com>
2026-04-11 14:37:04 -0600

    monitor silhouette + window passing implemented

---
+++ Assets/Scripts/SilhouetteManager.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using UnityEngine;
+
+public class SilhouetteManager : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private SpriteRenderer monitorRenderer;
+    [SerializeField] private SpriteRenderer windowRenderer;
+
+    [Header("Sprites")]
+    [SerializeField] private Sprite monitorSprite;
+
+    [SerializeField] private Sprite humanWindowSprite;
+    [SerializeField] private Sprite mimicWindowSprite;
+
+    [Header("Window Movement")]
+    [SerializeField] private Transform windowStartPoint;
+    [SerializeField] private Transform windowEndPoint;
+    [SerializeField] private float moveDuration = 7f;
+
+    private Coroutine moveCoroutine;
+
+    public void ShowMonitorSilhouette(bool isMimic)
+    {
+        monitorRenderer.sprite = monitorSprite;
+        monitorRenderer.enabled = true;
+    }
+
+    public void hideMonitorSilhouette()
+    {
+        monitorRenderer.enabled = false;
+    }
+
+    public void PlayWindowPass(bool isMimic)
+    {
+        if (moveCoroutine != null)
+            StopCoroutine(moveCoroutine);
+
+        moveCoroutine = StartCoroutine(WindowPassCoroutine(isMimic));
+    }
+
+    private IEnumerator WindowPassCoroutine(bool isMimic)
+    {
+        windowRenderer.sprite = isMimic ? mimicWindowSprite : humanWindowSprite;
+        windowRenderer.enabled = true;
+        windowRenderer.transform.position = windowStartPoint.position;
+
+        float time = 0f;
+        while (time < moveDuration)
+        {
+            time += Time.deltaTime;
+            float t = time / moveDuration;
+            windowRenderer.transform.position = Vector3.Lerp(windowStartPoint.position, windowEndPoint.position, t);
+            yield return null;
+        }
+
+        windowRenderer.transform.position = windowEndPoint.position;
+        windowRenderer.enabled = false;
+    }
+
+}
